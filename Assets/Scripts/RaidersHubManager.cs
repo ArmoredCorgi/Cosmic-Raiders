b28a99dd0f28fa83403d44584eccce44ae59b8dd
Nicholas Jorgensen <nicholas.k.jorgensen@gmail.com>
2019-01-22 11:56:02 -0700

    MultipleCamFeeds working

--- Assets/Scripts/RaidersHubManager.cs
+++ Assets/Scripts/RaidersHubManager.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class RaidersHubManager : MonoBehaviour {
-
-    [SerializeField] GameObject[] camMonitors;
-    [SerializeField] Material camFeedMaterial;
 
     GameObject[] securityCams;
     List<RenderTexture> camFeeds = new List<RenderTexture>();
@@ -25,16 +23,37 @@ public class RaidersHubManager : MonoBehaviour {
         securityCams = GameObject.FindGameObjectsWithTag("SecurityCam");
 
         //Set all cameras to correct render texture feed:
-        var count = 0;
+        var count = 1;
 
         foreach ( GameObject securityCam in securityCams )
         {
             var cam = securityCam.GetComponentInChildren<Camera>();
             var texture = new RenderTexture(1024, 1024, 16);
+            Material material = new Material( Shader.Find("Specular") );
+
+            AssetDatabase.CreateAsset(material, "Assets/Materials/CamFeed" + count + ".mat");
             
             cam.targetTexture = texture;
+            material.mainTexture = texture;
 
-            camFeedMaterial.mainTexture = texture;
+            var camFeedMonitor = GameObject.Find("CamFeed" + count);
+
+            Renderer monitorRenderer = camFeedMonitor.GetComponent<Renderer>();
+
+            monitorRenderer.material = material;
+
+            count++;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        var count = 1;
+
+        foreach ( GameObject securityCam in securityCams )
+        {
+            AssetDatabase.DeleteAsset("Assets/Materials/CamFeed" + count + ".mat");
+            count++;
         }
     }
 }
